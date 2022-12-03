59b9ec9bedf9491480a9159936f40902a7145b01
DenisKabanov <dskabanov@edu.hse.ru>
2022-12-03 17:18:20 +0300

    update run_all

--- run_all.sh
+++ run_all.sh
@@ -7,8 +7,8 @@ g++ alg1_no_par.cpp -o exe/alg1_no_par.out
 
 
 # g++ alg1_par.cpp -lpthread -o exe/alg1_par.out
-# ./exe/alg1_par.out arrs/array1.txt array2.txt 2
-# ./alg1_par.out arr1024x1024.txt arr1024x1024.txt 32
+# ./exe/alg1_par.out arrs/array1.txt arrs/array2.txt 2
+# ./exe/alg1_par.out arrs/arr1024x1024.txt arrs/arr1024x1024.txt 32
 
 # g++ alg2_no_par.cpp -o exe/alg2_no_par.out
 # ./exe/alg2_no_par.out arrs/arr64x64.txt arrs/arr64x64.txt
